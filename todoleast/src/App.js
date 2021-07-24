2d21115ae7ca15d0f57a64050c3f7cbd3ac3584d
MuniruIssah <imuniru@st.ug.edu.gh>
2021-07-24 21:51:21 +0000

    first commit

---
+++ todoleast/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <h1>To do Least</h1>
+       <button>Add a Todo</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
